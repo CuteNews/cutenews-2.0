1d8d5d1a76fcafc71a5ba5f26d05e15f3eeaf5c8
CuteNews <support@cutephp.com>
2013-09-18 09:02:12 -0400

    Autoupdater Robot

--- print.php
+++ print.php
@@ -4,6 +4,8 @@ require_once (dirname(__FILE__).'/core/init.php');
 
 list($id, $template, $popup) = GET('id, template, popup');
 
+$id = cn_id_alias($id);
+
 if (!$template) $template = 'default';
 $ent = db_news_load(db_get_nloc($id));
 
